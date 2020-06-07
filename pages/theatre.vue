65a5e7168315bbce926b043ff35c51cdce9a250f
andynuma <igreen.re317v10@gmail.com>
2020-06-07 22:54:08 +0900

    add layout

---
+++ pages/theatre.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>in Teatre</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator";
+
+@Component
+export default class theatre extends Vue {
+  layout() {
+    return "theatre";
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
